b3b63468d16dc8bade11c874505f5b43c9f764fa
huijingj <vanessajin0426@gmail.com>
2025-03-07 15:59:53 -0800

    Certifications section finished.

---
+++ src/components/features/Certifications/CertificateList.tsx
@@ -0,0 +1,47 @@
+import { Certificate } from "@/sections/Certifications";
+
+interface CertificateProps {
+  certificates: Certificate[];
+}
+
+export default function CertificateList({ certificates }: CertificateProps) {
+  return (
+    <div>
+      <ul className="flex flex-col gap-6 ">
+        {certificates.map((certificate, i) => (
+          <li key={i}>
+            <div className="flex flex-row justify-between max-m:flex-col">
+              <div className="flex flex-col gap-1">
+                <p className="font-dm-sans font-medium l:text-l text-m text-text-important-light">
+                  {certificate.title}
+                </p>
+                <p className="font-dm-sans font-medium l:text-l text-m text-text-light">
+                  {certificate.organization}
+                </p>
+                <p className="font-dm-sans font-normal l:text-l text-m text-text-light">
+                  {certificate.date}
+                </p>
+              </div>
+
+              <div>
+                <a
+                  href={certificate.link}
+                  className="relative text-accent-light font-dm-sans font-medium l:text-l text-m 
+             underline underline-offset-6 transition-all duration-300 
+             before:absolute before:left-0 before:bottom-0 before:h-0 before:w-full 
+             before:bg-accent-light before:transition-all before:duration-300 
+             before:origin-bottom hover:text-primary-light 
+             hover:before:h-full"
+                >
+                  <span className="relative z-10 hover:text-primary-light">
+                    Certificate
+                  </span>
+                </a>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- src/sections/Certifications.tsx
+++ src/sections/Certifications.tsx
@@ -1,3 +1,49 @@
+import CertificateList from "@/components/features/Certifications/CertificateList";
+
+export interface Certificate {
+  title: string;
+  organization: string;
+  date: string;
+  link: string;
+}
+
+const certificates: Certificate[] = [
+  {
+    title: "Software Engineer Bootcamp",
+    organization: "General Assembly",
+    date: "11/2024",
+    link: "/sde.pdf",
+  },
+  {
+    title: "UI UX Design Essentials",
+    organization: "Udemy",
+    date: "05/2024",
+    link: "/uiux.pdf",
+  },
+  {
+    title: "Meta Front-end Developer",
+    organization: "Coursera",
+    date: "08/2023",
+    link: "/coursera.pdf",
+  },
+  {
+    title: "Bachelor of Arts, Spanish",
+    organization: "University of California, Irvine",
+    date: "06/2018",
+    link: "/uci.pdf",
+  },
+];
+
 export default function Certifications() {
-  return <div>Certifications</div>;
+  return (
+    <div id="certifications" className="flex flex-col l:gap-8 gap-6">
+      <div>
+        <h1 className="text-secondary-light font-semibold font-playfair-display l:text-2xl m:text-xl s:text-l">
+          Certifications
+        </h1>
+      </div>
+
+      <CertificateList certificates={certificates} />
+    </div>
+  );
 }
